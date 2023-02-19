765330bc287798133c473713d12307cc56aade82
Vinod Mylapilli <vinodmylapilli380@gmail.com>
2023-02-19 20:05:49 +0530

    commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.secret_key = 'many random bytes'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'crud'
+
+mysql = MySQL(app)
+
+
+
+@app.route('/')
+def Index():
+
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM todos")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', todos = data)
+
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    if request.method == "POST":
+        flash("Data Inserted Successfully")
+        title = request.form['title']
+        description = request.form['description']
+        remarks = request.form['remarks']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO todos (title, description, remarks) VALUES (%s, %s, %s)", (title, description, remarks))
+        mysql.connection.commit()
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id_data>', methods = ['POST', 'GET'])
+def delete(id_data):
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM todos WHERE id=%s", (id_data))
+    mysql.connection.commit()
+    # cur.close()
+    return redirect(url_for('Index'))
+
+
+
+@app.route('/update', methods= ['POST', 'GET'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        title = request.form['title']
+        description = request.form['description']
+        remarks = request.form['remarks']
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+        UPDATE todos SET title=%s, description=%s, remarks=%s
+        WHERE id=%s
+        """, (title, description, remarks, id_data))
+        mysql.connection.commit()
+        flash("Data Updated Successfully")
+        return redirect(url_for('Index'))
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2023 at 12:18 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+CREATE DATABASE IF NOT EXISTS `crud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `crud`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `remarks` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `title`, `description`, `created_at`, `remarks`) VALUES
+(8, 'Learn Python', 'Whole Oops Concept and webscarping', '2023-02-17 16:30:28', 'Started'),
+(9, 'learn Django', 'models ', '2023-02-17 16:31:13', 'started'),
+(10, 'assessement', 'python and aws', '2023-02-17 16:31:50', 'ready to prepare');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
